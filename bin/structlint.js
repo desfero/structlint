c2e4a119a7ceedf50ffe1df37931484e9c9735ea
Paweł Lula <pavloblack@hotmail.com>
2020-02-29 23:16:58 +0800

    Add skip ignore option for ts-node to allow running structlint from node_modules

--- bin/structlint.js
+++ bin/structlint.js
@@ -4,5 +4,6 @@ const path = require("path");
 require("ts-node").register({
   transpileOnly: true,
   project: path.resolve(__dirname, "../tsconfig.json"),
+  skipIgnore: true,
 });
 require("../src/cli").run(process.argv.slice(2));
