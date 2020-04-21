4b2216c69e088e1cf7853534a840fdf76719daff
Paweł Lula <pavloblack@hotmail.com>
2020-04-22 00:36:28 +0200

    #8 Add support for node barrel imports

--- src/traversar.ts
+++ src/traversar.ts
@@ -2,7 +2,8 @@ import micromatch from "micromatch";
 
 import { File, DeepReadonly } from "./types";
 import { ImportConfig } from "./config";
-import { debug } from "./utils";
+import { debug, nonNullable } from "./utils";
+import { BABEL_PARSER, getParser } from "./parsers";
 
 const traversalDebug = debug("traversal");
 
@@ -28,6 +29,33 @@ const findFilesWithImports = (
     }),
   );
 
+/**
+ * For some environments we need to adjust glob to catch more env specific imports
+ *
+ * For e.g. node env has a concept of barrel imports (index.js) that can be imported
+ * just with folder name.
+ *
+ */
+const adjustGlobToParser = (filePath: string, glob: string) => {
+  const parser = getParser(filePath);
+
+  nonNullable(parser);
+
+  const scanInfo = micromatch.scan(glob)
+
+  switch (parser.name) {
+    case BABEL_PARSER:
+      // to support barrel imports remove `/*` from the glob if passed
+      if (scanInfo.glob.endsWith("/*")) {
+        return `${glob.slice(0, -2)}`;
+      }
+
+      return glob;
+    default:
+      return glob;
+  }
+};
+
 const findMatchedImports = (
   file: DeepReadonly<File>,
   importConfig: ImportConfig,
@@ -36,7 +64,12 @@ const findMatchedImports = (
     `Finding matched imports in ${file.path} for ${importConfig.glob} glob`,
   );
 
-  const matchedImports = micromatch(file.imports, importConfig.glob);
+  const glob = adjustGlobToParser(file.path, importConfig.glob);
+
+  const matchedImports = micromatch(
+    file.imports,
+    glob,
+  );
 
   traversalDebug(
     `Found ${matchedImports.length} matched imports.\n ${matchedImports.join(
